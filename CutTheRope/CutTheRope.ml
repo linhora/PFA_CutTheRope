942f6c96b857965fc99439b3bdb0a90f59e0bfd7
Joe De Oliveira <joe.de-oliveira@a-018730.ups.u-psud.fr>
2017-05-02 16:54:19 +0200

    physique balle+++

--- CutTheRope/CutTheRope.ml
+++ CutTheRope/CutTheRope.ml
@@ -1,38 +1,67 @@
-open Graphics;;
+open Graphics;; 
 
-let left = 0;;
-let right = 300;;
-let down = 0;;
-let up = 500;;
-
-let paddle = 100;;
-let thick = 4;;
 
 let ball = 30;;
-let mass = 0.001;;
-let bounce= 0.40;;
+let mass = 0.005;;
+let bounce= 0.7;;
 let maxVelocity= 15.0;;
 
 open_graph " 900x800"
 ;;
 
-
 Graphics.auto_synchronize false
 ;;
 
+type balle = {mutable position : float*float;mutable vitesse : float*float;masse : float; taille : float};; 
+
+let evalForces balle listForces = 
+  begin
+    let rec evaluation balle = function 
+      |[] -> []
+      |f :: [] -> f balle :: []
+      |f :: l -> f balle :: evaluation balle l
+    in
+    evaluation balle listForces 
+end ;;
+let sommeForceX listeForces balle = 
+  begin
+    let rec sommeX = function 
+      |[]->0.0
+      |(x,_)::l->x +. sommeX l
+    in
+    sommeX (evalForces balle listeForces)
+  end;;
+
+let sommeForceY listeForces balle = 
+  begin
+    let rec sommeY = function
+      |[]->0.0
+      |(_,y)::l->y+.sommeY l
+    in
+    sommeY (evalForces balle listeForces)
+  end;;
+    
+let nextFrame balle listForces=
+    begin
+      let acceleration = ((sommeForceX listForces balle) /. balle.masse,(sommeForceY listForces balle) /. balle.masse) in
+      let (ax,ay) = acceleration in
+      let (vx,vy) = balle.vitesse in
+      let (x,y) = balle.position in
+      balle.vitesse <- (vx+.ax,vy+.ay);
+      balle.position <- (x+.vx,y+.vy);
+    end;;
 
 
-let draw_ball x y =
-	fill_circle x y ball
+
+
+let draw_ball x y size =
+	fill_circle x y size
 ;;
 
 
-let calc_acceleration =
-	(0.0,-.mass)
 ;;
 
-
-
+(*
 let new_speed (vx,vy) (ax,ay) =
 	if(vy > -.maxVelocity)
 		then (vx+.ax,vy+.ay)
@@ -44,19 +73,25 @@ let new_position (bx,by) (vx,vy) =
 	(bx+.vx,by+.vy)
 ;;
 
-(*
+
 let detect_bounce (bx,by) (vx,vy) =
 	if by -. float_of_int ball/.2.0 < 1.0
 		then  (vx,(vy*. -.0.8))
 	else
 		(vx,vy)
 ;;
+
+
+let detect_bounce2 (bx,by) (vx,vy) platfrom =
+	if by -. float_of_int ball <= 0.0
+		then  ((bx+.(vx*.(abs_float (by-.(float_of_int ball))/.vy)),(float_of_int ball)+.((vy*. -.bounce)*.(abs_float (by-.(float_of_int ball))/.vy))),(vx,(vy*. -.bounce)))
+	else
+	((bx,by),(vx,vy))
+;;
 *)
 
-let abs_f x =
-	if x<0.0
-		then -.x
-	else x
+let calc_acceleration =
+	(0.0,-.mass)
 
 let detect_bounce (bx,by) (vx,vy) =
 	if by -. float_of_int ball <= 0.0
@@ -65,38 +100,27 @@ let detect_bounce (bx,by) (vx,vy) =
 	((bx,by),(vx,vy))
 ;;
 
+(*#####################################*)
 
 let rec wait n = 
 	if n=0 then Graphics.synchronize () else wait (n-1)
 ;;
 
 
-let loose y =
-	if y < 0
-		then exit 0
-;;
+let rec game balle =
 
-let rec game ((x,y),(vx,vy)) =
 	Graphics.clear_graph ();
-	draw_ball (int_of_float x) (int_of_float y);
+	let(x,y) = balle.position in 
+	let(vx,vy) = balle.vitesse in 
+	draw_ball (int_of_float x) (int_of_float y) (int_of_float balle.taille);
 	
 	
-	
-	(*loose y;*)
-	
-	wait 800000;
-	
-	Printf.printf "x: %f y: %f  p: %f vy: %f \n" x y vx vy;
+	wait 400000;
 	
 	
-	game (detect_bounce(new_position (x,y) (new_speed (vx, vy) calc_acceleration)) ((new_speed (vx,vy) calc_acceleration)))
+	nextFrame balle [fun balle -> (0.0,-.0.001)];
+	game balle
 	
-	(*	
-	game (new_position (x,y) (detect_bounce (x,y) (new_speed (vx, vy) calc_acceleration))) (detect_bounce (x,y) (new_speed (vx,vy) calc_acceleration))
-	*)
 ;;
 
-
-
-game ((100.0,400.0),(0.0,0.0));;
-
+game {position=(100.0,100.0);vitesse=(0.0,-0.05);masse=1.0;taille=10.0};;
---
+++ CutTheRope/CutTheRope_Old.ml
@@ -0,0 +1,102 @@
+open Graphics;;
+
+let left = 0;;
+let right = 300;;
+let down = 0;;
+let up = 500;;
+
+let paddle = 100;;
+let thick = 4;;
+
+let ball = 30;;
+let mass = 0.01;;
+let bounce= 0.80;;
+let maxVelocity= 15.0;;
+
+open_graph " 900x800"
+;;
+
+
+Graphics.auto_synchronize false
+;;
+
+
+
+let draw_ball x y =
+	fill_circle x y ball
+;;
+
+
+let calc_acceleration =
+	(0.0,-.mass)
+;;
+
+
+
+let new_speed (vx,vy) (ax,ay) =
+	if(vy > -.maxVelocity)
+		then (vx+.ax,vy+.ay)
+	else
+		(vx+.ax,vy+.ay)
+;;
+
+let new_position (bx,by) (vx,vy) =
+	(bx+.vx,by+.vy)
+;;
+
+(*
+let detect_bounce (bx,by) (vx,vy) =
+	if by -. float_of_int ball/.2.0 < 1.0
+		then  (vx,(vy*. -.0.8))
+	else
+		(vx,vy)
+;;
+*)
+
+let abs_f x =
+	if x<0.0
+		then -.x
+	else x
+
+let detect_bounce (bx,by) (vx,vy) =
+	if by -. float_of_int ball <= 0.0
+		then  ((bx+.(vx*.(abs_float (by-.(float_of_int ball))/.vy)),(float_of_int ball)+.((vy*. -.bounce)*.(abs_float (by-.(float_of_int ball))/.vy))),(vx,(vy*. -.bounce)))
+	else
+	((bx,by),(vx,vy))
+;;
+
+
+let rec wait n = 
+	if n=0 then Graphics.synchronize () else wait (n-1)
+;;
+
+
+let loose y =
+	if y < 0
+		then exit 0
+;;
+
+let rec game ((x,y),(vx,vy)) =
+	Graphics.clear_graph ();
+	draw_ball (int_of_float x) (int_of_float y);
+	
+	
+	
+	(*loose y;*)
+	
+	wait 800000;
+	
+	Printf.printf "x: %f y: %f  p: %f vy: %f \n" x y vx vy;
+	
+	
+	game (detect_bounce(new_position (x,y) (new_speed (vx, vy) calc_acceleration)) ((new_speed (vx,vy) calc_acceleration)))
+	
+	(*	
+	game (new_position (x,y) (detect_bounce (x,y) (new_speed (vx, vy) calc_acceleration))) (detect_bounce (x,y) (new_speed (vx,vy) calc_acceleration))
+	*)
+;;
+
+
+
+game ((100.0,600.0),(0.0,0.0));;
+
